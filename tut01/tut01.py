b17b0d79ccf04122c06cfd8b14e89075f9d0f7be
Kavita124 <kavitameena15082001@gmail.com>
2022-09-15 19:25:18 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -25,3 +25,15 @@ def oct(x,y,z):
                 return 3 #octane 3
             else :
                 return -3 #octane -3
+
+#reading the input file
+df = pd.read_csv("octant_input.csv") 
+
+#editing excel file
+df.at[0,'u_avg']=df['U'].mean() 
+df.at[0,'v_avg']=df['V'].mean()
+df.at[0,'w_avg']=df['W'].mean()
+
+df['U-u_avg']=df['U']-df.at[0,'u_avg']
+df['V-v_avg']=df['V']-df.at[0,'v_avg']
+df['W-w_avg']=df['W']-df.at[0,'w_avg']

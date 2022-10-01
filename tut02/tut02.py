c7d677c1b8e43dac5735c99916a2cc6d2b08e9c5
Kavita124 <103851335+Kavita124@users.noreply.github.com>
2022-10-01 00:07:09 -0700

    Update tut02.py
    column_defining

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -14,10 +14,31 @@ df = pd.read_csv("octant_input.csv")
 #-----------------------------------------------------
 
 # Mean of U
-U_avg = df['U'].mean()
+U_mean = df['U'].mean()
 
 # Mean of V
-V_avg = df['V'].mean()
+V_mean = df['V'].mean()
 
 # Mean of W
-W_avg = df['W'].mean()
+W_mean = df['W'].mean()
+
+# defining columns
+df["U_mean"] = U_mean
+# this line is there to print U_mean once, else it would have filled all the rows with the same value(U_mean)
+df["U_mean"] = df["U_mean"].head(1)
+
+df["V_mean"] = V_mean
+df["V_mean"] = df["V_mean"].head(1)
+
+df["W_mean"] = W_mean
+df["W_mean"] = df["W_mean"].head(1)
+
+#-----------------------------------------------------
+
+U_minus_U_mean = (df['U'] - U_mean)
+V_minus_V_mean = (df['V'] - V_mean)
+W_minus_W_mean = (df['W'] - W_mean)
+
+df["U' = U - U_mean"] = U_minus_U_mean
+df["V' = V - V_mean"] = V_minus_V_mean
+df["W' = W - W_mean"] = W_minus_W_mean

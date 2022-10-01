b00b163390b23858fd901f5a836b8a5a44b4cda0
Kavita124 <103851335+Kavita124@users.noreply.github.com>
2022-09-30 23:50:36 -0700

    taking_mean

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,5 +1,4 @@
-def octant_transition_count(mod=5000):
-###Code
+import pandas as pd
 
 from platform import python_version
 ver = python_version()
@@ -8,6 +7,17 @@ if ver == "3.8.10":
     print("Correct Version Installed")
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
 mod=5000
-octant_transition_count(mod)
+
+df = pd.read_csv("octant_input.csv")
+
+#-----------------------------------------------------
+
+# Mean of U
+U_avg = df['U'].mean()
+
+# Mean of V
+V_avg = df['V'].mean()
+
+# Mean of W
+W_avg = df['W'].mean()
